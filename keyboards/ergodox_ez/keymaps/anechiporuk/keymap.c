8d20129844dd53c5a2037bb46065de782c3dbf1c
Alexander Nechiporuk <3927292+anechiporuk@users.noreply.github.com>
2023-05-29 17:29:56 +0200

    update keyboard layout

--- keyboards/ergodox_ez/keymaps/anechiporuk/keymap.c
+++ keyboards/ergodox_ez/keymaps/anechiporuk/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "keymap_dvorak.h"
 
 // #define _______ KC_TRNS
 // #define XXXXXXX KC_NO
@@ -11,17 +12,13 @@
 #define OSM_RALT OSM(MOD_RALT)
 #define OSM_RSFT OSM(MOD_RSFT)
 #define OSM_RGUI OSM(MOD_RGUI)
+#define OSL_SYMB OSL(Symbols)
+#define OSL_NUMP OSL(NumPad)
+#define OSL_SYST OSL(System)
+#define OSL_MEDI OSL(Media)
 
-#define GRAVE_ESC_GUI_OVERRIDE
-#define GRAVE_ESC_CTRL_OVERRIDE
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-#define AUTO_SHIFT_MODIFIERS
-#define AUTO_SHIFT_NO_AUTO_REPEAT
-#define NO_AUTO_SHIFT_ALPHA
 
-#undef AUTO_SHIFT_TIMEOUT
-#define AUTO_SHIFT_TIMEOUT 175
 #undef TAPPING_TERM
 #define TAPPING_TERM 175
 
@@ -29,54 +26,54 @@ enum custom_keycodes {
     KC_P000 = SAFE_RANGE,
 };
 
-enum dvorak_keycodes {
-    DV_BSLS = KC_BSLS,
-    DV_COLN = S(KC_Z),
-    DV_COMM = KC_W,
-    DV_DOT  = KC_E,
-    DV_DQUO = S(KC_Q),
-    DV_EQL  = KC_RBRC,
-    DV_LABK = S(KC_W),
-    DV_LBRC = KC_MINS,
-    DV_LCBR = KC_UNDS,
-    DV_MINS = KC_QUOT,
-    DV_PIPE = KC_PIPE,
-    DV_PLUS = KC_RCBR,
-    DV_QUES = KC_LCBR,
-    DV_QUOT = KC_Q,
-    DV_RABK = S(KC_E),
-    DV_RBRC = KC_EQL,
-    DV_RCBR = KC_PLUS,
-    DV_SCLN = KC_Z,
-    DV_SLSH = KC_LBRC,
-    DV_UNDS = KC_DQUO,
-    DV_A = KC_A,
-    DV_B = KC_N,
-    DV_C = KC_I,
-    DV_D = KC_H,
-    DV_E = KC_D,
-    DV_F = KC_Y,
-    DV_G = KC_U,
-    DV_H = KC_J,
-    DV_I = KC_G,
-    DV_J = KC_C,
-    DV_K = KC_V,
-    DV_L = KC_P,
-    DV_M = KC_M,
-    DV_N = KC_L,
-    DV_O = KC_S,
-    DV_P = KC_R,
-    DV_Q = KC_X,
-    DV_R = KC_O,
-    DV_S = KC_SCLN,
-    DV_T = KC_K,
-    DV_U = KC_F,
-    DV_V = KC_DOT,
-    DV_W = KC_COMM,
-    DV_X = KC_B,
-    DV_Y = KC_T,
-    DV_Z = KC_SLSH,
-};
+// enum dvorak_keycodes {
+//     DV_BSLS = KC_BSLS,
+//     DV_COLN = S(KC_Z),
+//     DV_COMM = KC_W,
+//     DV_DOT  = KC_E,
+//     DV_DQUO = S(KC_Q),
+//     DV_EQL  = KC_RBRC,
+//     DV_LABK = S(KC_W),
+//     DV_LBRC = KC_MINS,
+//     DV_LCBR = KC_UNDS,
+//     DV_MINS = KC_QUOT,
+//     DV_PIPE = KC_PIPE,
+//     DV_PLUS = KC_RCBR,
+//     DV_QUES = KC_LCBR,
+//     DV_QUOT = KC_Q,
+//     DV_RABK = S(KC_E),
+//     DV_RBRC = KC_EQL,
+//     DV_RCBR = KC_PLUS,
+//     DV_SCLN = KC_Z,
+//     DV_SLSH = KC_LBRC,
+//     DV_UNDS = KC_DQUO,
+//     DV_A = KC_A,
+//     DV_B = KC_N,
+//     DV_C = KC_I,
+//     DV_D = KC_H,
+//     DV_E = KC_D,
+//     DV_F = KC_Y,
+//     DV_G = KC_U,
+//     DV_H = KC_J,
+//     DV_I = KC_G,
+//     DV_J = KC_C,
+//     DV_K = KC_V,
+//     DV_L = KC_P,
+//     DV_M = KC_M,
+//     DV_N = KC_L,
+//     DV_O = KC_S,
+//     DV_P = KC_R,
+//     DV_Q = KC_X,
+//     DV_R = KC_O,
+//     DV_S = KC_SCLN,
+//     DV_T = KC_K,
+//     DV_U = KC_F,
+//     DV_V = KC_DOT,
+//     DV_W = KC_COMM,
+//     DV_X = KC_B,
+//     DV_Y = KC_T,
+//     DV_Z = KC_SLSH,
+// };
 
 enum layers {
     BASE,
@@ -91,33 +88,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY Layer for DVORAK layout
  *
  *  ,--------------------------------------------------.                 ,--------------------------------------------------.
- *  |   \    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                 |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   =    |
+ *  |   GEsc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                 |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   =    |
  *  |--------+------+------+------+------+-------------|                 |------+------+------+------+------+------+--------|
- *  |   Tab  |  '"  |  ,<  |  .>  |   P  |   Y  |      |                 |      |   F  |   G  |   C  |   R  |   L  |   /    |
+ *  |   Tab  |  '"  |  ,<  |  .>  |   P  |   Y  |  (   |                 |  )   |   F  |   G  |   C  |   R  |   L  |   /    |
  *  |--------+------+------+------+------+------|      |                 |      |------+------+------+------+------+--------|
- *  |  GEsc  |   A  |   O  |   E  |   U  |   I  |------|                 |------|   D  |   H  |   T  |   N  |   S  |   -    |
+ *  |    \   |   A  |   O  |   E  |   U  |   I  |------|                 |------|   D  |   H  |   T  |   N  |   S  |   -    |
  *  |--------+------+------+------+------+------|      |                 |      |------+------+------+------+------+--------|
- *  | LShft( |   ;  |   Q  |   J  |   K  |   X  |      |                 |      |   B  |   M  |   W  |   V  |   Z  | RShft) |
+ *  | LShft  |   ;  |   Q  |   J  |   K  |   X  |      |                 |      |   B  |   M  |   W  |   V  |   Z  | RShft  |
  *  `--------+------+------+------+------+-------------'                 `-------------+------+------+------+------+--------'
- *    | LCtrl|      | LGui | LAlt | Media|                                             |   {  |   }  |   [  |   ]  |      |
+ *    | LCtrl|      |      | LGui | Media|                                             |   {  |   }  |   [  |   ]  | LCtrl|
  *    `----------------------------------'                                             `----------------------------------'
  *                                        ,-------------.               ,-------------.
  *                                        | Ins  |      |               |      |      |
  *                                 ,------|------|------|               |------+------+------.
  *                                 |      |      |      |               |      |      |      |
  *                                 | Spc  | Del  |------|               |------|BckSpc|Enter |
- *                                 |      |      | Sym  |               | Sym  |      |      |
+ *                                 |      |      | LAlt |               | Sym  |      |      |
  *                                 `--------------------'               `--------------------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
-    DV_BSLS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,           KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   DV_EQL,
-    KC_TAB,   DV_QUOT,  DV_COMM,  DV_DOT,   DV_P,     DV_Y,     XXXXXXX,         XXXXXXX,  DV_F,     DV_G,     DV_C,     DV_R,     DV_L,     DV_SLSH,
-    QK_GESC,  DV_A,     DV_O,     DV_E,     DV_U,     DV_I,                                DV_D,     DV_H,     DV_T,     DV_N,     DV_S,     DV_MINS,
-    SC_LSPO,  DV_SCLN,  DV_Q,     DV_J,     DV_K,     DV_X,     XXXXXXX,         XXXXXXX,  DV_B,     DV_M,     DV_W,     DV_V,     DV_Z,     SC_RSPC,
-    OSM_LCTL, XXXXXXX,  OSM_LGUI, OSM_LALT, OSL(Media),                                              DV_LCBR,  DV_RCBR,  DV_LBRC,  DV_RBRC,  XXXXXXX,
+    QK_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,           KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   DV_EQL,
+    KC_TAB,   DV_QUOT,  DV_COMM,  DV_DOT,   DV_P,     DV_Y,     DV_LPRN,         DV_RPRN,  DV_F,     DV_G,     DV_C,     DV_R,     DV_L,     DV_SLSH,
+    DV_BSLS,  DV_A,     DV_O,     DV_E,     DV_U,     DV_I,                                DV_D,     DV_H,     DV_T,     DV_N,     DV_S,     DV_MINS,
+    OSM_LSFT, DV_SCLN,  DV_Q,     DV_J,     DV_K,     DV_X,     XXXXXXX,         XXXXXXX,  DV_B,     DV_M,     DV_W,     DV_V,     DV_Z,     OSM_RSFT,
+    OSM_LCTL, XXXXXXX,  XXXXXXX,  OSM_LGUI, OSL_MEDI,                                                DV_LCBR,  DV_RCBR,  DV_LBRC,  DV_RBRC,  OSM_RCTL,
                                                       KC_INS,   XXXXXXX,         XXXXXXX,  XXXXXXX,
                                                                 XXXXXXX,         XXXXXXX,
-                                            KC_SPC,   KC_DEL,   OSL(Symbols),    OSL(Symbols),KC_BSPC,KC_ENT
+                                            KC_SPC,   KC_DEL,   OSM_LALT,        OSL_SYMB,KC_BSPC,KC_ENT
 ),
 /* Symbols Layer
  *
